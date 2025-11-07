1ebb8730c277cf876ed6f77cc23ce8edfd5fd8a9
unknown <m.umarfarooq1175@gmail.com>
2025-11-07 10:50:15 +0530

    backend

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,7 +3,7 @@ require ('dotenv').config ();
 
 let connectToDb = async () => {
   try {
-    await mongoose.connect (process.env.MONGO_URL);
+    await mongoose.connect (`${process.env.MONGODB_URI}`);
     console.log ('Connected to Db');
   } catch (error) {
     console.log ('Database connection error:', error.message);
